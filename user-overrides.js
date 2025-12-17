6e15528264775d327aa2b509da9ac6ca75e12a77
Constantine Chen <csc_dev@protonmail.com>
2025-12-16 22:06:59 -0800

    Added user overrides.

---
+++ user-overrides.js
@@ -0,0 +1,3 @@
+user_pref("privacy.resistFingerprinting", true); // [FF41+]
+user_pref("privacy.resistFingerprinting.letterboxing", true); // [HIDDEN PREF]
+user_pref("privacy.resistFingerprinting.pbmode", true); // [FF114+]
